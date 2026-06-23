39f5d679607199c4bb006e9d4a1e21f91f106cf3
Dikshi Sriram <178579382+Dikshithaa556@users.noreply.github.com>
2026-06-23 21:48:56 +0530

    Added Missing Number folder

---
+++ MissingNumber/XorApproach.java
@@ -0,0 +1,15 @@
+package MissingNumber;
+
+public class XorApproach {
+    public int findNum(int[] nums) {
+        int xor = 0;
+        int n = nums.length;
+
+        for (int i = 0; i <= n; i++) {
+            xor ^= i;
+            xor ^= nums[i];
+        }
+
+        return xor;
+    }
+}
