da3bffa141db7539d7b6d355e076646ec5e4a559
Dikshi Sriram <178579382+Dikshithaa556@users.noreply.github.com>
2026-06-27 20:02:43 +0530

    Added Transform Array

---
+++ Arrays/TransformArray.java
@@ -0,0 +1,13 @@
+package Arrays;
+
+public class TransformArray {
+    public int[] transformArray(int[] nums){
+        int n = nums.length;
+        int[] res = new int[n];
+        for(int i=0; i<n; i++){
+            int k = ((i+nums[i])%n + n)%n;
+            res[i] = nums[k];
+        }
+        return res;
+    }
+}
